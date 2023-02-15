eaff34390dd41a2d6b03f3a38c85c10cfe1e7132
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2023-02-15 10:16:51 -0500

    5

--- vite-project/rolling.js
+++ vite-project/rolling.js
@@ -1,10 +1,44 @@
+import "./style.css";
+
 const getRandomNumber = (min, max) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
 //calls function getRandomNumber, get "randam" numbers between 1-6 (both numbers are included)
 const rollDice = () => getRandomNumber(1, 6);
 
-document
+const formDetails = document.getElementById("submitted");
+
+formDetails.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  // gets User input on player names
+  const player1 = document.getElementById("player1").value;
+  const player2 = document.getElementById("player2").value;
+
+  // roll dice for both players
+  const player1Score = rollDice();
+  const player2Score = rollDice();
+
+  // Empty string to store result from later
+  let result = "";
+
+  // determine the result
+  if (player1Score > player2Score) {
+    result = `${player1} won the round`;
+  } else if (player2Score > player1Score) {
+    result = `${player2} won the round`;
+  } else {
+    result = "This round is tied";
+  }
+
+  // display the result on the page
+  document.getElementById("results").innerHTML = `
+  <p>${player1} => ${player1Score}</p>
+  <p>${player2} => ${player2Score}</p>
+  <p>${result}</p>
+  `;
+});
+
+/* document
   .getElementById("submitted")
   .addEventListener("submit", function (event) {
     // gets User input on player names
@@ -34,7 +68,7 @@ document
   <p>${result}</p>
   `;
     event.preventDefault();
-  });
+  }); */
 
 /* function logSubmit(event) {
   log.textContent = `hi`;
